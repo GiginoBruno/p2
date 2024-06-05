1c425a0adbb694936c77191f0dcfd440719949e6
GiginoBruno <l.bruno41@studenti.unisa.it>
2024-06-05 13:39:23 +0200

    Id-3 resolved

--- src/it/unisa/control/LoginServlet.java
+++ src/it/unisa/control/LoginServlet.java
@@ -16,48 +16,33 @@ import it.unisa.model.*;
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/Login")
-public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
-			
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	UserDao usDao = new UserDao();
-		
-		try
-		{	    
 
-		     UserBean user = new UserBean();
-		     user.setUsername(request.getParameter("un"));
-		     user.setPassword(request.getParameter("pw"));
-		     user = usDao.doRetrieve(request.getParameter("un"),request.getParameter("pw"));
-			   		    
-		    
-		     String checkout = request.getParameter("checkout");
-		     
-		     if (user.isValid())
-		     {
-			        
-		          HttpSession session = request.getSession(true);	    
-		          session.setAttribute("currentSessionUser",user); 
-		          if(checkout!=null)
-		        	  response.sendRedirect(request.getContextPath() + "/account?page=Checkout.jsp");
-		          
-		          else
-		        	  response.sendRedirect(request.getContextPath() + "/Home.jsp");
-		     }
-			        
-		     else 
-		          response.sendRedirect(request.getContextPath() +"/Login.jsp?action=error"); //error page 
-		} 
-				
-				
-		catch(SQLException e) {
-			System.out.println("Error:" + e.getMessage());
-		}
-		  }
-	}
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserDao usDao = new UserDao();
+        
+        try {    
+            String un = request.getParameter("un");
+            String pw = request.getParameter("pw");
+            UserBean user = usDao.doRetrieve(un, pw);
+            
+            String checkout = request.getParameter("checkout");
+            
+            if (user != null && user.isValid()) {
+                HttpSession session = request.getSession(true);    
+                session.setAttribute("currentSessionUser", user); 
+                
+                if (checkout != null)
+                    response.sendRedirect(request.getContextPath() + "/account?page=Checkout.jsp");
+                else
+                    response.sendRedirect(request.getContextPath() + "/Home.jsp");
+            } else {
+                response.sendRedirect(request.getContextPath() + "/Login.jsp?action=error");
+            }
+        } catch(SQLException e) {
+            System.out.println("Error:" + e.getMessage());
+            // Gestire l'errore in modo appropriato, ad esempio, reindirizzare a una pagina di errore generica
+            response.sendRedirect(request.getContextPath() + "/error.jsp");
+        }
+    }
+}
